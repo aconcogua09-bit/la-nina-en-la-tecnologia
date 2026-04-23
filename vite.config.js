896c619ead086de9feb22f4a904f72cdd43033a1
aconcogua09-bit <aconcogua09@gmail.com>
2026-04-22 21:59:31 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/la-nina-en-la-tecnologia/',
   plugins: [react(), tailwindcss()],
 })
